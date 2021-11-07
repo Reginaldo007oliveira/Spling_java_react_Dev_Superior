1d4429a993ccb01597e18ebe7b6f9d6f99124217
Reginaldo <reginaldo007oliveira@gmail.com>
2021-11-07 07:30:54 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -22,23 +22,18 @@ public class Seller {
     this.id = id;
     this.name = name;
   }
-
   public Long getId() {
     return id;
   }
-
   public void setId(Long id) {
     this.id = id;
   }
-
   public String getName() {
     return name;
   }
-
   public void setName(String name) {
     this.name = name;
   }
-
   public List<Sale> getSales() {
     return sales;
   }
